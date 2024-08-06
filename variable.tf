10dc7a45f1abff83eccbf6c10850f9c413ccb579
ff <ffvb178@gmail.com>
2024-08-05 19:05:47 -0500

    setting provider, key and some variables

---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    type = string
+    default = "us-east-2"
+}
+
+variable "key_name" {
+    type = string
+    default = "ff"
+}
+
+variable "key" {
+  type = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDHiEjmjUluSBKtGaGRmT+nsPtPZtjhYH5V5ZGtWbf2CZUeInSXk1/I195pnKKCOP8eZmFBw4daS0Cp+FSbMlbVRS3XiKYpX40h8degfOkBEQYSzmQwo1we3h4jaYffGskiXeqPejqBI9ia9qQnT3BVda4yeRl6ziG4kwUJas/5ROCH4CCcIk9C2FrW2NmpEV6fQgWMxMKFO0ogPZsp5xaVF5qcUMHE09Ce3Ujli6F5VP8yv/ixS2TZPOejC3RsY4jH4pVNbNldZJVUVasrOG9ZV8iUZW+WGkFc5eFBkwd8amh4U1sBCRqLVlJ/CvhBcoB6wobpYc+U4Z8QhPzm2ydxfuWWkw95dphXGTPU/vRXuXYdn2W6WRHVPoBQZzbpVkjifAEuLw1/D/MdNQP7jj4GhADdVIK9SHx0NmigutXF6trAr5pvde5TVWi2IGzG7qw6CitqMt4wujACU1Qn2JjzOYjoVr8ruxnHp/KFB0urg/q9vFfZ/MbYCfGmBtltZ/k= 12245@LAPTOP-RBFMU6TJ"
+}
